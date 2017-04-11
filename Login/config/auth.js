8428ee729ba23373cf9391f96c90cbcf11ffb2bd
priyankathakur1234 <pri02.thakur693@gmail.com>
2017-04-11 22:51:14 +0530

    login with facebook

---
+++ Login/config/auth.js
@@ -0,0 +1,8 @@
+module.exports = {
+	'facebookAuth' :{
+  'clientID' : '799381703558071',
+  'clientSecret' : 'd31f06aab21dedcd6be33f9c63de1836',
+  'callbackURL' : 'http://localhost:8000/auth/facebook/callback',
+ // 'profileFields'   : ['emails']
+}
+}
